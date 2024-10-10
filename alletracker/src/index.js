93a31d89b53dc4ab31a21a05edd1daa361b70ed9
JJakimcz <jjakimc@gmail.com>
2024-10-10 16:51:25 +0200

    pre alpha version start

--- alletracker/src/index.js
+++ alletracker/src/index.js
@@ -4,7 +4,7 @@ async function main() {
   try {
 	let phrase = 'konsola xbox one'
     const offers = await getOffers(0,2,phrase);
-    console.log('User Information:', userInfo);
+    console.log('User Information:', offers);
   } catch (error) {
     console.error('Error fetching user information:', error.message);
   }
