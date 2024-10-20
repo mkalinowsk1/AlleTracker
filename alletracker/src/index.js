1a99510721ff320bff72b55b2f932615589720a0
mkalinowsk1 <mical7360@gmail.com>
2024-10-20 23:51:29 +0200

    basic database connection

--- alletracker/src/index.js
+++ alletracker/src/index.js
@@ -1,3 +1,25 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017', { 
+}).then(() => {
+	console.log('Connected to MongoDB...');
+}).catch(err => {
+	console.error('Could not connect to MongoDB...');
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json())
+
+
+
+
 const { getOffers } = require('./api');
 
 async function main() {
@@ -9,5 +31,7 @@ async function main() {
     console.error('Error fetching user information:', error.message);
   }
 }
-
-main();
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}...`);
+});
+//main();
