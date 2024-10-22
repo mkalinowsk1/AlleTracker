d83552cf9fca882f553184cb9992c772cede5025
mkalinowsk1 <mical7360@gmail.com>
2024-10-23 01:02:42 +0200

    path fix

--- alletracker/src/index.js
+++ alletracker/src/index.js
@@ -4,7 +4,7 @@ const app = express();
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-const Product = require('../models/Product');
+const Product = require('./models/Product');
 
 mongoose.connect('mongodb://localhost:27017/alletracker', { 
 }).then(() => {
